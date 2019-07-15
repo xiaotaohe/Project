e85aaf25a94b6dbed06a1fb647a4398ebdfc33b7
xiaotaohe <taochao1997@qq.com>
2019-07-15 02:35:32 -0700

    服务端

---
+++ 黑名单/Sys_server.cpp
@@ -0,0 +1,39 @@
+/*************************************************************************
+  > File Name: Sys_server.cpp
+  > Author: 陶超
+  > Mail: taochao1997@qq.com 
+  > Created Time: Mon 08 Jul 2019 10:24:45 PM PDT
+ ************************************************************************/
+
+#include"tcp.hpp"
+#include"mysql.hpp"
+#include"Sys_server.hpp"
+#include<iostream>
+#include<stdio.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<sys/fcntl.h>
+#include<sys/types.h>
+#include<stdlib.h>
+#include<json/json.h>
+#include<string.h>
+#include<atomic>
+#include<map>
+#include<event.h>
+using namespace std;
+
+
+int main()
+{
+  //1.启动服务器
+  Server server("0.0.0.0",9090);
+  //2.连接数据库
+  mysql data_base;
+  if(!data_base.connect ("root","1230","black_list"))
+  {
+    cout<<"connect data_bas error!"<<endl;
+    exit(0);
+  }
+  //3.libevent
+  work::lib_event(&server,&data_base); 
+}
